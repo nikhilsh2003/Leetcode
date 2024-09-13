2ffd364cd2f928e60c3fb741b4218b9d9c2062ac
Nikhil Sharma <85234162+nikhilsh2003@users.noreply.github.com>
2024-09-13 07:09:30 +0530

    Time: 796 ms (37.71%), Space: 22.8 MB (84.83%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
@@ -22,25 +22,28 @@ public:
             }
         }
     }
-    long long factorial(int n) {
-        return (n==1 || n==0) ? 1: n*1ll * factorial(n - 1);
+    long long factorial(int n,vector<long long> &factorialStore) {
+        if(factorialStore[n]!=-1) return factorialStore[n];
+        else
+        return factorialStore[n]=(n==1 || n==0) ? 1: n*1ll * factorial(n - 1,factorialStore);
     }
     long long countGoodIntegers(int n, int k) {
         set<string> uniqueFreq;
         generatePalindromes(0,n,uniqueFreq,"",k);
+        vector<long long> factorialStore(n+1,-1);
         long long ans=0;
         for(auto &x:uniqueFreq) {
             vector<int> freq(10,0);
             for(auto &c:x) freq[c-'0']++;
-            long long temp1=factorial(n),temp2=0;
+            long long temp1=factorial(n,factorialStore),temp2=0;
             for(int i=0;i<=9;i++) {
-                temp1/=factorial(freq[i]);
+                temp1/=factorial(freq[i],factorialStore);
             } 
             if(freq[0]!=0) {
-                temp2=factorial(n-1);
-                temp2/=factorial(freq[0]-1);
+                temp2=factorial(n-1,factorialStore);
+                temp2/=factorial(freq[0]-1,factorialStore);
                 for(int i=1;i<=9;i++) {
-                    temp2/=factorial(freq[i]);
+                    temp2/=factorial(freq[i],factorialStore);
                 }
             }
             ans+=temp1-temp2;
